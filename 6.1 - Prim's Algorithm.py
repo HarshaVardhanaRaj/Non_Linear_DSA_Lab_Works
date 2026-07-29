f023f740ad6be953d063f2fc09978fa40eb56ae3
P Harsha Vardhana Raj <106435268+HarshaVardhanaRaj@users.noreply.github.com>
2026-07-29 11:20:28 +0530

    Enhance Prim's algorithm with explanatory comments
    Added comments to explain the Prim's algorithm implementation.

--- 6.1 - Prim's Algorithm.py
+++ 6.1 - Prim's Algorithm.py
@@ -1,29 +1,30 @@
 import heapq
 
 def prim(graph):
-    visited = set()
-    min_heap = [(0, 1, None)]
+    #initialisations
+    visited = set()    #contains the visited nodes as a set
+    min_heap = [(0, 1, None)]    #contains the node's (weight, name, parent) for current nodes
+    mst = []    #is the minimum search tree
+    total_weight = 0    #is the total cost
 
-    mst = []
-    total_weight = 0
+    while min_heap:    #when min_heap is not empty... ie. when there are nodes to be visited
+        weight, current, parent = heapq.heappop(min_heap)    #weight, name and parent of current node is taken
 
-    while min_heap:
-        weight, current, parent = heapq.heappop(min_heap)
+        if current in visited:    #is current node was already visited
+            continue    #it skips the iteration
 
-        if current in visited:
-            continue
-
+        #if current node was not visited
         visited.add(current)
 
-        if parent is not None:
-            mst.append((parent, current, weight))                            
-            total_weight += weight
+        if parent is not None:    #is current node is not the start node
+            mst.append((parent, current, weight))    #add the weighted edge for the current node, from its parent
+            total_weight += weight    #increment the total weight
 
-        for neighbor, w in graph[current]:
-            if neighbor not in visited:
-                heapq.heappush(min_heap, (w, neighbor, current))
+        for neighbor, w in graph[current]:    #while the current node has got neighbours with weighted edge connection
+            if neighbor not in visited:    #if neighbour is not already visited
+                heapq.heappush(min_heap, (w, neighbor, current))    #add the details of the neighbour to min_heap
 
-    return mst, total_weight
+    return mst, total_weight    #return the mst and total weight
 
 graph = {
          0: [(1, 2), (3, 6)],
